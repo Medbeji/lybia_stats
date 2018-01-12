06fadd79010930a096d72c3b8efc89fcb02b21e3
MedBeji <medbeji@MacBook-Pro-de-MedBeji.local>
2018-01-12 21:28:33 +0100

    Initial Commit

---
+++ LybiaApp/Controller/HomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeVC.swift
+//  LybiaApp
+//
+//  Created by MedBeji on 12/01/2018.
+//  Copyright © 2018 unfpa. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setupBtn()
+    }
+    
+    func setupBtn(){
+        
+        // Set the corner radius for the image
+        
+        
+        // Give btn a shadow
+        
+    }
+
+
+}
