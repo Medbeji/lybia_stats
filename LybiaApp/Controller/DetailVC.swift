0eef08e7550fc7b40470adb89f51a1d1aaa38a58
MedBeji <medbeji@MacBook-Pro-de-MedBeji.local>
2018-01-13 00:15:33 +0100

    Added slide up menu

--- LybiaApp/Controller/DetailVC.swift
+++ LybiaApp/Controller/DetailVC.swift
@@ -12,11 +12,16 @@ class DetailVC: UIViewController {
 
     @IBOutlet weak var btnView: UIView!
     @IBOutlet weak var btn: UIButton!
+    var isSlideUp: Bool = true
+    var menuBottomAnchor: NSLayoutConstraint?
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupBtnMenu()
+        self.view.addSubview(menuView)
+        menuBottomAnchor = self.menuView.anchorWithReturnAnchors(nil, left: self.view.leftAnchor, bottom: self.view.bottomAnchor, right: self.view.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: -242, rightConstant: 0, widthConstant: 0, heightConstant: 242)[1]
     }
     
     func setupBtnMenu(){
@@ -31,15 +36,41 @@ class DetailVC: UIViewController {
         btnView.layer.shadowRadius = 4.0
     }
     
-   
-    @IBAction func slideUp(_ sender: Any) {
-        
+    let menuView: UIView =  {
+       let view = UIView()
+        view.backgroundColor = UIColor.init(red: 68/255, green: 87/255, blue: 106/255, alpha: 1)
+        //Shadow
+        view.layer.shadowColor = UIColor.lightGray.cgColor
+        view.layer.shadowOffset = .zero
+        view.layer.shadowOpacity = 1.5
+        view.layer.masksToBounds = false
+        view.layer.shadowRadius = 4.0
+        return view
+    }()
     
-        UIView.transition(with: sender as! UIView, duration: 1.5, options: .transitionFlipFromRight, animations: {
-            self.btnView.layer.position.y = self.btnView.layer.position.y - 250
-            self.btnView.transform = self.btnView.transform.rotated(by: CGFloat.init(Double.pi))
+
+    
+    @IBAction func slideUp(_ sender: Any) {
+        // Slide up btn
+        UIView.transition(with: sender as! UIView, duration: 1.0, options: .transitionFlipFromRight, animations: {
+           self.slideUpBtn()
         }, completion: { (Succ) in
+            
         })
     }
     
+    func slideUpBtn(){
+        if self.isSlideUp {
+            self.btnView.layer.position.y = self.btnView.layer.position.y - 250
+            self.menuView.layer.position.y = self.menuView.layer.position.y - 242
+          
+        } else {
+            self.btnView.layer.position.y = self.btnView.layer.position.y + 250
+            self.menuView.layer.position.y = self.menuView.layer.position.y + 242
+
+        }
+        self.isSlideUp = !self.isSlideUp
+        self.btnView.transform = self.btnView.transform.rotated(by: CGFloat.init(Double.pi))
+    }
+ 
 }
