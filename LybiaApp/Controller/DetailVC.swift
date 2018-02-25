3ea08bda85d1ab2ba25ed5afcec01432510a1a84
MedBeji <medbeji@MacBook-Pro-de-MedBeji.local>
2018-02-25 21:40:34 +0100

    Finished the menu view

--- LybiaApp/Controller/DetailVC.swift
+++ LybiaApp/Controller/DetailVC.swift
@@ -38,42 +38,37 @@ class DetailVC: UIViewController {
             return lb
         }()
     
-        let sectionTitle : UILabel = {
+        lazy var  sectionTitle : UILabel = {
             let lb           = UILabel()
             lb.text          = "القطاع"
             lb.font          = UIFont.init(name: "Farah", size: 22)
             lb.textAlignment = .center
+            lb.textColor     = .white
+            lb.frame         = CGRect(x: 0, y: 130, width: self.view.frame.width, height: 30)
             return lb
         }()
     
-        let ageHorizontalLine: UIView = {
-            let hl = UIView()
-            hl.backgroundColor = .lightGray
-            hl.alpha = 0.5
-            return hl
-        }()
-        let sectionHorizontalLine: UIView = {
-            let hl = UIView()
-            hl.backgroundColor = .lightGray
-            return hl
-        }()
+    
+    
     
         let agePickerView = CustomUIPickerView()
+        let sectionPickerView = CustomUIPickerView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupMenu()
-        setupAgePickerView()
+        setupPickerView(pickerView: agePickerView,y: 44)
+        setupPickerView(pickerView: sectionPickerView,y:160)
     }
     
-    func setupAgePickerView(){
-        agePickerView.widthComponent = self.view.frame.width
-        agePickerView.delegate = agePickerView.self
-        agePickerView.dataSource = agePickerView.self
-        agePickerView.transformToHorizontale()
-        menuView.addSubview(agePickerView)
-        agePickerView.frame = CGRect(x: 0 - 300, y: 44  , width: view.frame.width + 600, height: 75)
-        agePickerView.selectRow(1, inComponent: 0, animated: true)
+    func setupPickerView(pickerView: CustomUIPickerView,y: CGFloat){
+        pickerView.widthComponent = self.view.frame.width
+        pickerView.delegate = pickerView.self
+        pickerView.dataSource = pickerView.self
+        pickerView.transformToHorizontale()
+        menuView.addSubview(pickerView)
+        pickerView.frame = CGRect(x: 0 - 300, y: y  , width: view.frame.width + 600, height: 75)
+        pickerView.selectRow(1, inComponent: 0, animated: true)
     }
     
     func setupMenu(){
@@ -91,11 +86,8 @@ class DetailVC: UIViewController {
         menuBottomAnchor = self.menuView.anchorWithReturnAnchors(nil, left: self.view.leftAnchor, bottom: self.view.bottomAnchor, right: self.view.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: -242, rightConstant: 0, widthConstant: 0, heightConstant: 242)[1]
         // Add ageTitle to menu
         self.menuView.addSubview(ageTitle)
-        self.menuView.addSubview(ageHorizontalLine)
+        self.menuView.addSubview(sectionTitle)
         ageTitle.anchor(menuView.topAnchor, left: menuView.leftAnchor, bottom: nil, right: menuView.rightAnchor, topConstant: 8, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 30)
-//        ageHorizontalLine.anchor(ageTitle.bottomAnchor, left: nil, bottom: nil, right: nil, topConstant: 1, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 120, heightConstant: 1)
-//        
-//        ageHorizontalLine.anchorCenterXToSuperview()
      
     }
     
